fe4d1c8651ee07232b33f4251360eea9547c436e
Sarthak Sahu <vishalsahu5@gmail.com>
2019-09-11 01:13:21 +0530

    String Task

--- A-118.cs
+++ A-118.cs
@@ -11,10 +11,20 @@ namespace codeforces
             string s = Console.ReadLine();
             string ans = "";
             HashSet<char> vowels = new HashSet<char>();
+            vowels.Add('A');
+            vowels.Add('E');
+            vowels.Add('I');
+            vowels.Add('O');
+            vowels.Add('U');
+            vowels.Add('Y');
             for (int i = 0; i < s.Length; i++)
             {
+                if (!vowels.Contains(Char.ToUpper(s[i])))
+                {
+                    ans = ans + "." + Convert.ToString(Char.ToLower(s[i]));
+                }
             }
-
+            Console.WriteLine(ans);
         }
     }
 }
