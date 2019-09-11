add9c3a443115e37cc138c114ca02cfc5f2d99c8
Sarthak Sahu <vishalsahu5@gmail.com>
2019-09-12 00:19:43 +0530

    Domino Piling

---
+++ A_50.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeforces
+{
+    class A_50
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            Console.WriteLine((int.Parse(input[0]) * int.Parse(input[1]))/2);
+            //Console.ReadLine();
+       }
+    }
+}
